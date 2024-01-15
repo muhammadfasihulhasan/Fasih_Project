189486558c4376ddf39bd2ada07e8209c6153cf4
muhammadfasihulhasan <155235452+muhammadfasihulhasan@users.noreply.github.com>
2024-01-15 13:48:29 +0100

    Data Exploration

--- code.py
+++ code.py
@@ -28,6 +28,59 @@ df['WindDir3pm'] = LabelEncoder.fit_transform(df['WindDir3pm'])
 df['RainToday'] = LabelEncoder.fit_transform(df['RainToday'])
 df['RainTomorrow'] = LabelEncoder.fit_transform(df['RainTomorrow'])
 
+#Now exploring/visualizing data using different types of plots 
+#Firstly a  scatter plot showing Mininmum temperature vs the Maximum temperature in which the categorical variable will be Rain Tomorrow
+from pandas.plotting import scatter_matrix
+#plt.figure(figsize=(8,8))
+#sns.scatterplot(x='MaxTemp', y='MinTemp', hue='RainTomorrow', palette='inferno', data=df)
+
+#Secondly a piechart for representing the data in the columnn named 'Rain Today'
+import plotly.express as px
+#px.pie(df , names= 'RainToday')
+
+#Thirdly a line plot showing Humidity vs the temperature at 9 am in which the categorical variable will be Rain Tomorrow
+#sns.lineplot(x = 'Humidity9am',y = 'Temp9am', hue = 'RainTomorrow', palette = 'deep' , data = df)
+
+#Now checking  how strongly different variables are correlated with eachother using heatmap
+#The column Location has string values so dropping it for performing correlation
+#a = df.drop(['Location'], axis=1)
+#plt.figure(figsize=(12,12))
+#sns.heatmap(a.corr(), annot=True, linewidths=0.1, fmt=".2f")
+
+#Now creating a count plot showing the count of occurrences for each Location
+#plt.figure(figsize=(15,15))
+#x_loc = sns.countplot(x='Location', data= df, palette= 'Set1')
+#plt.xticks(rotation = 90)
+#plt.title('Location Count')
+
+#Now creating a count plot showing the count of occurrences of Rain to be on the next day or not
+#x_tom = sns.countplot(x='RainTomorrow', data= df, palette= 'Set2')
+#plt.title('Rain Tomorrow Count')
+
+#Now using a scatterplot for the relationship between both minimum and maximum temperatures with rainfall
+#plt.figure(figsize=(12,8))
+#plt.scatter (df['MinTemp'], df["Rainfall"],color ="deeppink")
+#plt.scatter (df['MaxTemp'], df["Rainfall"],color ="mistyrose")
+#plt.title('Comparison of temperatures with rain')
+#plt.xlabel(' temp(°C)')
+#plt.ylabel('rain(mm)')
+#plt.xticks(range(-10,50,2), rotation = 30)
+#plt.legend(['Min temperature','Max temperature'],loc = 'upper right')
+
+#Now for the relationship between Location and Rainfall
+#plt.figure(figsize=(16,12))
+#plt.scatter (df["Location"], df["Rainfall"],color ="deeppink")
+#plt.title("'Comparison location with rain'")
+#plt.xlabel("location")
+#plt.ylabel("Rain(mm)")
+#plt.xticks(rotation = 90)
+#plt.yticks(range(0,350,20))
+#plt.grid()
+
+
+
+
+
 
 
 
